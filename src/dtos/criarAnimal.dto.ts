d54d9630a9aa813b8358910f23e3835cbcf3e256
laurinha38 <lils1@aluno.ifal.edu.br>
2025-12-10 11:53:29 -0300

    commit

--- src/dtos/criarAnimal.dto.ts
+++ src/dtos/criarAnimal.dto.ts
@@ -1,8 +1,11 @@
 export class CriarAnimal {
+  id: number;
   nome: string;
   genero: string;
   raca: string;
   peso: number;
   idade: number;
   descricao: string;
+  url: string;
+  situacao: string;
 }
--- src/entidades/animal.ts
+++ src/entidades/animal.ts
@@ -1,10 +1,13 @@
 class Animal {
+  id: number;
   nome: string;
   genero: string;
   raca: string;
   peso: number;
   idade: number;
   descricao: string;
+  url: string;
+  situacao: string;
 }
 
 export { Animal };
